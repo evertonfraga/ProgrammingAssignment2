be214ea8b364aa614640b3af3b3c5542e828b20d
Everton Fraga <evertonfrag@gmail.com>
2015-04-26 17:59:41 -0300

    Programming Assignment #2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function returns an list containing the following functions:
+## 1) get: returns the matrix that was set on beforehand
+## 2) set: replaces the existing matrix with a new one
+## 3) getInverse: Returns the inverse matrix, but does not calculate it.
+##    The actual calculations are made on cacheSolve function
+## 4) setInverse: Setting the inversed matrix cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # Anitializing internal cache variable
+  inverseMatrixCache <- NULL
 
+  get <- function(){ x }
+    
+  set <- function(newMatrix){
+    # Assigning new value to internal variable
+    x <<- newMatrix
+    
+    # When my matrix is replaced/changed, we must clean the cache
+    inverseMatrixCache <<- NULL
+  }
+  
+  getInverse <- function(){ inverseMatrixCache }
+  
+  setInverse <- function(inverse){
+    inverseMatrixCache <<- inverse
+  }
+  
+  # Exposing functions to public calls
+  list(get = get,
+       set = set,
+       getInverse = getInverse,
+       setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes a makeCacheMatrix object as parameter and performs the following:
+## 1) Checks if are there any value on makeCacheMatrix > inverseMatrixCache variable.
+##    If there aren't any, calculate and store its value inside the makeCacheMatrix object.
+## 2) Returns the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # It will only calculate the inverted matrix if the cache doesn't exist
+  if (is.null(x$getInverse())){
+  
+    # Printing a message informing that the cache is inexistent and that's being calculated.
+    print("Cache MISS - Calculating inverse matrix...")
+    
+    # Assigning inverse matrix value to cached variable
+    x$setInverse(solve(x))
+  }
+  
+  # Both if inverse matrix was calculated or grabbed from cache, its value will be returned
+  x$getInverse()
 }
